d55cb8aa5113d78251f029f6f42844f2bb689e37
Yakumo Saki <yakumo@ziomatrix.org>
2020-06-29 11:46:31 +0900

    deployするか否かを先に聞くようにした

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 echo "Script dir is ${SCRIPT_DIR}"
 
+read -p "Deploy to AWS Lambda? Yes: Build + Deploy, No: Build only (y/N): " yn
+
 cd ${SCRIPT_DIR}
 
 rm -rf ${SCRIPT_DIR}/build/*
@@ -11,7 +13,6 @@ pip install --target ./build -r requirements.txt
 
 rsync -a -exclude=.git --exclude=package --exclude=deploy.sh --exclude=requirements.txt --exclude=function.zip --exclude=build ${SCRIPT_DIR}/. ${SCRIPT_DIR}/build/
 
-read -p "Deploy to AWS Lambda ? (y/N): " yn
 case "$yn" in [yY]*) ;; *) echo "Abort." ; exit ;; esac
 
 echo "Deploy to lambda using lambroll"
